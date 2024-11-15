67773c2584884ea448fedad9f43c3a1dd943ddeb
Anil Kumar <arbhutale@MacBookPro.lan>
2024-11-15 09:02:14 -0800

    iventory qr & billing added

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -30,7 +30,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("peradmin/", admin.site.urls),
-    path("admin/", my_admin_site.urls),
+    path("wifi_in/", my_admin_site.urls),
     path(
         "api-playground/",
         schema_view.with_ui("swagger", cache_timeout=0),
